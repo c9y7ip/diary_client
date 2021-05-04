d6f2daf41409263d5ef4c3c063733373ba90b60a
raymond <c9y7ip@gmail.com>
2021-05-04 00:32:14 -0700

    first commit

--- src/component/accountant/secondaryTable.jsx
+++ src/component/accountant/secondaryTable.jsx
@@ -1,12 +1,143 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
 import "../../styles/accountant.css";
 import { Fab, TableContainer } from "@material-ui/core";
-import AddIcon from "@material-ui/icons/Add";
+import { DataGrid, ukUA } from "@material-ui/data-grid";
+import CheckIcon from "@material-ui/icons/Check";
+import ClearIcon from "@material-ui/icons/Clear";
+import axios from "axios";
 
-function secondaryTable() {
+function SecondaryTable() {
+  const [items, setItems] = useState([]);
+  const [inputArray, setInputArray] = useState([]);
+  const [inputItem, setInputItem] = useState("");
+  const [inputPrice, setInputPrice] = useState("");
+  const [sum, setSum] = useState(0);
+
+  // calculation
+  const addition = (a, b) => {
+    return Number(a) + Number(b);
+  };
+
+  const totalSum = () => {
+    var result = items.reduce((a, v) => {
+      return addition(a, v.price);
+    }, 0);
+
+    setSum(result);
+  };
+
+  // unhide input box
+  const showInputBox = () => {
+    if (inputArray.length === 0) {
+      setInputArray([...inputArray, { item: "item", price: "price" }]);
+    } else {
+      return;
+    }
+  };
+
+  //hide input box
+  const removeInputBox = () => {
+    setInputArray([]);
+  };
+
+  // add data to database
+  const appendInput = async () => {
+    if (inputItem === "" || inputPrice === "") {
+      return;
+    } else {
+      const item = {
+        id: inputItem,
+        price: inputPrice,
+        action: "Delete",
+      };
+      console.log(item);
+
+      axios.post("http://localhost:5000/addNessaray", item).then((res) => {
+        console.log(res.data);
+        totalSum();
+        window.location.reload();
+      });
+
+      setInputItem("");
+      setInputPrice("");
+      setInputArray([]);
+    }
+  };
+
+  // useEffect update item table
+  useEffect(() => {
+    axios.get("http://localhost:5000/getNessarary").then((res) => {
+      setItems(res.data);
+      totalSum();
+    });
+  }, [items]);
+
   return (
     <div className="middle-section">
       <p className="title">Secondary Expenses</p>
+      {/* item table */}
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          columns={[
+            {
+              field: "id",
+              headerAlign: "center",
+            },
+            {
+              field: "price",
+              headerAlign: "center",
+            },
+            {
+              field: "action",
+              headerAlign: "center",
+            },
+          ]}
+          rows={items}
+          pageSize={5}
+          disableSelectionOnClick
+          isCellEditable={true}
+          onCellClick={(a, b, c) => {
+            if (a.value === "Delete") {
+              console.log(a.id, "this is aid");
+              console.log(items, "i can call");
+
+              axios.delete("http://localhost:5000/deleteNessaray", {
+                data: { id: a.id },
+              });
+
+              totalSum();
+            }
+          }}
+        />
+      </div>
+
+      {/* input box ordering */}
+      {inputArray.map((input) => (
+        <div className="wrapper" key={input.item}>
+          <input
+            className="inputBox"
+            type="text"
+            placeholder="item"
+            onChange={(e) => {
+              setInputItem(e.target.value);
+            }}
+          />
+          <input
+            className="inputBox"
+            type="number"
+            placeholder="0"
+            onChange={(e) => {
+              setInputPrice(e.target.value);
+            }}
+          />
+          <CheckIcon className="icon" onClick={appendInput} />
+          <ClearIcon className="icon" onClick={removeInputBox} />
+        </div>
+      ))}
+
+      {/* Show total sum */}
+      <h3 className="total">Total : {sum}</h3>
       <Fab className="addButton" size="small" color="primary" aria-label="add">
         <AddIcon />
       </Fab>
@@ -14,4 +145,4 @@ function secondaryTable() {
   );
 }
 
-export default secondaryTable;
+export default SecondaryTable;
